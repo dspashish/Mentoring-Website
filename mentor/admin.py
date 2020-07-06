25ad02ae7e0bd67cfb787d1db2c6dd3c6f83af36
dspashish <mrpycoder@gmail.com>
2020-07-06 05:28:48 +0000

    first commit

--- mentor/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import HomeWork, discuss, information
-
-
-admin.site.register(HomeWork)
-admin.site.register(discuss)
-admin.site.register(information)
